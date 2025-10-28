b5560826152e82f3a3cb03e539548cdde1e80cfb
martheh1 <marthe.h1@schule.bremen.de>
2025-10-28 20:32:53 +0100

    Enhance Project Management: Added new endpoints for project operations and managing employee assignments. Introduced integration tests for comprehensive coverage.

--- src/test/java/de/szut/lf8_starter/testcontainers/AbstractIntegrationTest.java
+++ src/test/java/de/szut/lf8_starter/testcontainers/AbstractIntegrationTest.java
@@ -1,10 +1,10 @@
 package de.szut.lf8_starter.testcontainers;
 
-import de.szut.lf8_starter.hello.HelloRepository;
-import org.junit.jupiter.api.BeforeEach;
+import de.szut.lf8_starter.config.TestConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,16 +18,9 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 @ActiveProfiles("it")
 @ContextConfiguration(initializers = PostgresContextInitializer.class)
-public class AbstractIntegrationTest {
+@Import(TestConfig.class)
+public abstract class AbstractIntegrationTest {
 
     @Autowired
     protected MockMvc mockMvc;
-
-    @Autowired
-    protected HelloRepository helloRepository;
-
-    @BeforeEach
-    void setUp() {
-        helloRepository.deleteAll();
-    }
 }
